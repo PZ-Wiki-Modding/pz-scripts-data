3982885b19979d0121a2d8de357ff1e6e828b03b
SimKDT <simon.cadet79@hotmail.fr>
2026-03-08 23:52:13 +0100

    Change doc style, remove build folder

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,9 @@
 import os
 import sys
 
-# Add parent directory to path
+# Add parent directory and docs directory to path
 sys.path.insert(0, os.path.abspath('../../'))
+sys.path.insert(0, os.path.abspath('../'))
 
 project = 'Project Zomboid Scripts API Reference'
 copyright = '2026, Scripts-Data Contributors'
@@ -21,7 +22,7 @@ extensions = [
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
 html_title = 'Project Zomboid Scripts API Reference'
 html_logo = None
@@ -29,16 +30,20 @@ html_favicon = None
 
 # Sphinx RTD Theme options
 html_theme_options = {
-    'logo_only': False,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': '#2980B9',
+    # 'logo_only': False,
+    # 'prev_next_buttons_location': 'bottom',
+    # 'style_external_links': False,
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': '#2980B9',
     'collapse_navigation': True,
-    'sticky_navigation': True,
+    # 'sticky_navigation': True,
     'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False,
+    # 'includehidden': True,
+    # 'titles_only': False,
+    "external_links": [
+        {"name": "GitHub", "url": "https://github.com/SirDoggyJvla/pz-scripts-data"},
+        {"name": "ZedScripts", "url": "https://pzwiki.net/wiki/ZedScripts"},
+    ],
 }
 
 # LaTeX output options
