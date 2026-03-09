da1cdf2287c2bdf76b1a9e3a53690b6d7909f003
SimKDT <simon.cadet79@hotmail.fr>
2026-03-09 23:15:50 +0100

    Build 42.15 updated translation data

---
+++ scripts/patch/patchTranslationFiles.py
@@ -0,0 +1,20 @@
+# update to 42.15
+import os, yaml
+
+translation_files_path = os.path.join(os.path.dirname(__file__), "../../data/translation_files")
+
+for file in os.listdir(translation_files_path):
+    if file.endswith(".yaml"):
+        with open(os.path.join(translation_files_path, file), "r") as f:
+            data = yaml.safe_load(f)
+
+        print(data["name"])
+
+        if "filePrefix" in data:
+            # Update the fileName field
+            data["fileName"] = data["filePrefix"]
+            data.pop("filePrefix", None)
+            data.pop("fileStarter", None)
+
+        with open(os.path.join(translation_files_path, file), "w") as f:
+            yaml.dump(data, f, sort_keys=False)
